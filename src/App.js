8dd0d2e379756448455e7a7cfffe6a9b127b128f
Keegan Lange <lange.keegan@gmail.com>
2022-02-15 17:30:29 -0600

    Created form input component and button component
    created form input component and button component along with the scss for each of them. Button component not complete just formed base structure and styles of the component.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
-import HomePage from './pages/homepage/homepage.component.jsx';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
-
-const temp = () => (
-  <div>
-    <h1>Temporary Item</h1>
-  </div>
-);
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
+import Header from "./components/header/header.component.jsx";
 
 function App() {
   return (
     <div>
       <Header />
-      <Switch>  
-        <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop' component={ShopPage}/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUp} />
       </Switch>
-      
     </div>
   );
 }
